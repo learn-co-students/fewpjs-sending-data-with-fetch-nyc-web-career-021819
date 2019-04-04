4866d073fee634a8add124632482aae34330e61e
Kate Kim <katekim5809@gmail.com>
2019-04-04 08:03:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
-const testVar = {}
 
-function testFunc() {
-  return "hi"
-}
+let formData = {
+  'firstname': 'Ella'
+  'registryMessage': 'Mo food no cry'
+};
+
+let configObj ={
+  method; 'POST'
+  data: formData
+};
+
+
+function registerSelf(){
+  return fetch("http://guestbook.example.com/register", configObj)
+    .then((response)) => {
+      return response.json();
+    })
+    .then((json)=>{
+    consol.log(json['message'])
+    return json['message'];
+  });
+
+
+function errorSelf(){
+  return fetch("http://guestbook.example.com/register", configObj)
+  .then((response) => {
+    return response.json()
+  })
+  .then((json) => {
+    consol.log(json['message'])
+    return json['message'];
+  })
+  .catch((error) => {
+    return ":(";
+  })
+};
+
+  const testVar = {}
+
+  function testFunc() {
+    return "hi"
+  }

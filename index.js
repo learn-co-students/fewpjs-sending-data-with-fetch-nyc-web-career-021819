b19797f4319c67bfdc6f5358b2be963ea973a293
Jessy Hu <jesshuy12@gmail.com>
2019-04-03 21:25:51 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,36 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+let formData = {
+  "firstName": "Jessy",
+  "registryMessage": "Hello"
+};
+
+let configObj = {
+  method: "POST",
+  body: formData
+}
+
+function registerSelf() {
+  return fetch("http://guestbook.example.com/register", configObj)
+  .then(function(response) {
+    return response.json()
+  })
+  .then(function(json){
+    return json.message
+  })
+}
+
+function errorSelf() {
+  return fetch("http://guestbook.example.com/register-error", configObj)
+  .then(function(response) {
+    return response.json()
+  })
+  .then(function(json) {
+    return json.message
+  })
+  .catch(function(error) {
+    return ":("
+  })
+}

baf3f9ca2d05ea3e98049e79f7c9fe22429b18cb
Khaled Khalifeh <khaled.khalifeh@gmail.com>
2019-04-04 14:27:26 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,35 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function registerSelf() {
+  let firstName = 'khaled';
+  let rMessage = 'RANDOM MESSAGE'
+
+  return fetch('http://guestbook.example.com/register', {
+    method: 'POST',
+    body: {
+      "firstName": `${firstName}`,
+      "registryMessage": `${rMessage}`
+    }
+  })
+  .then((res) => {return res.json()})
+  .then((json) => {return json.message})
+}
+
+function errorSelf() {
+  let firstName = 'khaled';
+  let rMessage = 'RANDOM MESSAGE'
+
+  return fetch('http://guestbook.example.com/register-error', {
+    method: 'POST',
+    body: {
+      "firstName": `${firstName}`,
+      "registryMessage": `${rMessage}`
+    }
+  })
+  .then((res) => {return res.json()})
+  .then((json) => {return json.message})
+  .catch((err) => {return ':('})
+
+}

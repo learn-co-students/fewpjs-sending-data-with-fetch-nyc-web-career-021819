9bfef5d764e40d46a6d1e18b1cf4992aa3e13f03
github username <github email address>
2019-04-04 12:26:11 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,38 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+let configObj = {
+  "method": "POST",
+  "body": formData
+}
+
+let formData = {
+  "firstName": "Will",
+  "registryMessage": "Hello World"
+}
+
+function registerSelf() {
+  return fetch('http://guestbook.example.com/register', configObj)
+    .then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      return json.message;
+    })
+}
+
+function errorSelf() {
+  return fetch('http://guestbook.example.com/register-error', {
+    "method": "POST",
+    "body": {
+      "firstName": "will",
+      "registryMessage": "Hi"
+    }
+  }).then(function(response) {
+    return response.json()
+  }).then(function(json) {
+    return json.message
+  }).catch(function(error) {
+    return ":("
+  })
+}
